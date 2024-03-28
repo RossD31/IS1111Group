5181fed9212956f5169e04854ccecc0248583307
Praise Wenegieme <praisewenegieme29@gmail.com>
2024-03-28 17:08:49 +0000

    Added in database for the description page

--- Detailspage.py
+++ Detailspage.py
@@ -1,7 +1,9 @@
 import customtkinter as ctk
 from PIL import Image, ImageTk
 from page1 import DeliveryCollection as DC
-
+import random
+import csv
+import phonenumbers
 
 
 
@@ -41,12 +43,79 @@ class GUI_badnumber():
         self.logo.place(x=0,y=0)
 
     def submit(self):
-        first_name=self.fname_entry.get()
-        last_name=self.lname_entry.get()
-        if first_name and last_name:
-            print(first_name,last_name)
-        else:
-            print("False")
+        #Check if the inputs for first and last name are good
+        fname_valid = self.validation_name_checker(self.fname_entry.get())
+        lname_valid = self.validation_name_checker(self.lname_entry.get())
+
+        # Check if the inputs for area code and number are valid
+        area_code_valid = self.validate_area_code(self.area_code.get())
+        number_valid = self.validate_phone_number(self.number.get())
+
+        # If all inputs are valid, proceed
+        if fname_valid and lname_valid and area_code_valid and number_valid:
+            receipt_number = self.generate_receipt_number()
+
+            # Collect all valid inputs
+            data = {
+                "Receipt Number": receipt_number,
+                "First Name": self.fname_entry.get(),
+                "Last Name": self.lname_entry.get(),
+                "Area Code": self.area_code.get(),
+                "Phone Number": self.number.get()
+            }
+
+            # Write data to CSV file
+            with open('customer-details.csv', mode='a', newline='') as file:
+                writer = csv.DictWriter(file, fieldnames=data.keys())
+
+                # If the file is empty, write headers
+                if file.tell() == 0:
+                    writer.writeheader()
+
+                # Write data to the file
+                writer.writerow(data)
+       
+    
+    def validate_area_code(self, country_input):
+        try:
+            # Check if the country code is valid
+            if not phonenumbers.region_code_for_country_code(int(country_input)):
+                return False
+        except ValueError:
+            return False
+        return True
+    
+    def validate_phone_number(self, number):
+        if not number.isdigit() or len(number) != 9:
+            return False
+        return True
+
+    def get_customer_info_by_receipt(self, receipt_number):
+        # Search for customer information by receipt number
+        with open('customer-details.csv', mode='r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if row["Receipt Number"] == receipt_number:
+                    return {
+                        "Receipt Number": row["Receipt Number"],
+                        "First Name": row["First Name"],
+                        "Last Name": row["Last Name"],
+                        "Area Code": row["Area Code"],
+                        "Phone Number": row["Phone Number"]
+                    }
+        return None  # Return None if receipt number not found
+
+    def generate_receipt_number(self):
+        # Generate a random receipt number between 1 and 10000
+        receipt_number = random.randint(1, 10000)
+        return receipt_number
+    
+    def validation_name_checker(self, name):
+        if len(name.strip()) > 30 or len(name.strip()) == 0:
+            #We can change this print statement to something else later
+            print("\nSorry your name is too long long.")
+            return False
+        return True
 
         
 
---
+++ database-logic.py
@@ -0,0 +1,28 @@
+from random import randint
+
+"""
+My Goal is to connect page 2 with the relevant database information
+
+How will i go about that
+
+1.Use a random module to get a receipt number which will act as a key for both database information customer and order
+
+2.Write back from the database the relevant information into the console or GUI
+
+3. Refactor and Optimize code if necessary
+
+
+I am using the random module here but it may be better to add that in later for when they actually click submit
+The detail variables are sample variables and will be modified when we reach the pa
+"""
+def write_customer_details():
+    pass
+
+def write_order_details():
+    pass
+
+def get_customer_details():
+    pass
+
+def get_order_details():
+    pass
